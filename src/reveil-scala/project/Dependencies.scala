adb6e3112faa9ff38d1c7c41b88661d725a8b0e2
FrancoisRmn <francoisromain4@gmail.com>
2021-11-15 15:27:15 +0100

    update mqtt

--- src/reveil-scala/project/Dependencies.scala
+++ src/reveil-scala/project/Dependencies.scala
@@ -2,7 +2,12 @@ import sbt._
 
 object Dependencies {
 
+	val AkkaVersion = "2.6.14"
+
 	val dependencies = List (
-		"com.github.hyjay" %% "scala-mqtt-client" % "0.1.0"
+		//"com.github.hyjay" %% "scala-mqtt-client" % "0.1.0",
+		//"com.softwaremill.sttp.client3" %% "core" % "3.3.16",
+		"com.lightbend.akka" %% "akka-stream-alpakka-mqtt" % "3.0.3",
+		"com.typesafe.akka" %% "akka-stream" % AkkaVersion
 	)
 }
--- src/reveil-scala/src/main/scala/main.scala
+++ src/reveil-scala/src/main/scala/main.scala
@@ -1,36 +1,54 @@
-import com.github.hyjay.mqtt.core._
-import com.github.hyjay.mqtt.netty.NettyMqttClient
+import akka.actor.ActorSystem
+import akka.stream._
+import akka.Done
+import akka.stream.alpakka.mqtt.{MqttConnectionSettings, MqttMessage, MqttQoS, MqttSubscriptions}
+import akka.stream.alpakka.mqtt.scaladsl.MqttSource
+import akka.stream.scaladsl.{Keep, Sink, Source}
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
 
-import scala.concurrent.Await
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.DurationInt
+import scala.concurrent.duration.{Duration, SECONDS}
+import scala.concurrent.{Await, Future}
+
 
 object Main {
 	def main(args: Array[String]): Unit = {
 		MQTTConnection()
 		System.exit(0)
+
 	}
 
 	private def MQTTConnection(): Unit = {
-		val client  = new NettyMqttClient("192.168.0.125", 1883, tls = false)
+		val connectionSettings = MqttConnectionSettings(
+			"tcp://192.168.0.125:1883", // (1)
+			"test-scala-client", // (2)
+			new MemoryPersistence // (3)
+		)
 
-		val TOPIC = "test/message"
 
-		val result = for {
-			_ <- client.connect(CONNECT("CLIENT_ID"))
-			_ = client.send(SUBSCRIBE(Seq((TOPIC, 0))))
-			suback <- client.pull()
-			pub <- client.pull()
-		} yield pub
+		val mqttSource: Source[MqttMessage, Future[Done]] =
+			MqttSource.atMostOnce(
+				connectionSettings.withClientId(clientId = "scala"),
+				MqttSubscriptions("test/message", MqttQoS.AtLeastOnce),
+				bufferSize = 16
+			)
 
-		val receivedResult = Await.result(result, 15.seconds)
+		implicit val system: ActorSystem = ActorSystem("reveil")
+		implicit val materializer: ActorMaterializer = ActorMaterializer()
 
-		convertToInt(receivedResult)
+		val (_, result) = mqttSource
+			.take(1)
+			.toMat(Sink.seq)(Keep.both)
+			.run()
 
-		client.send(DISCONNECT())
+		Await.ready(result, Duration(30, SECONDS))
+
+		val index = convertToInt(result.toString)
+
+		println(index)
+		//todo websocket pour renvoyer index
 	}
 
-	private def convertToInt(receivedResult: Packet): Unit = {
+	private def convertToInt(receivedResult: String): Int = {
 		// PUBLISH(test/message,WrappedArray(56, 32, 49, 49, 32, 49, 50, 32, 56, 32, 49, 49, 32, 49, 50, 32),-1,false,0,false)
 
 		//Utilisation d'une regex pour garder que les données
@@ -38,7 +56,7 @@ object Main {
 
 		//On ne garde que les chiffres sous la forme :
 		// 56, 32, 49, 49, 32, 49, 50, 32, 56, 32, 49, 49, 32, 49, 50,
-		val dataAscii = (dataPattern findAllIn receivedResult.toString).mkString(",")
+		val dataAscii = (dataPattern findAllIn receivedResult).mkString(",")
 
 		//On supprime la dernière virgule
 		val cleanDataAscii = dataAscii.slice(0, dataAscii.length - 2).replaceAll("\\s", "")
@@ -62,11 +80,16 @@ object Main {
 			}
 		} yield data
 
-		val numberPattern = "(\\d)".r
-		val numbersClean = (numberPattern findAllIn numbers.toString).mkString(",")
+		val values = numbers.mkString("").split(" ").toList
 
-		val data = numbersClean.split(",")
-
-		println(data.mkString(","))
+		if (values(4).toInt < 15 && values(5).toInt < 25)
+			1
+		else if (values(4).toInt < 30 && values(5).toInt < 50)
+			2
+		else if (values(4).toInt < 55 && values(5).toInt < 90)
+			3
+		else if (values(4).toInt < 110 && values(5).toInt < 180)
+			4
+		else 5
 	}
 }
