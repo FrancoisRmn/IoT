48d5a817041a4fe6ab8e21a2d65115117cf3b244
FrancoisRmn <francoisromain4@gmail.com>
2021-12-06 10:29:08 +0100

    Update hour.py

--- src/raspberry/hour.py
+++ src/raspberry/hour.py
@@ -1,6 +1,8 @@
 import time
 import datetime
 
+import RPi.GPIO as GPIO
+
 from threading import Thread
 from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
@@ -13,18 +15,34 @@ serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial,cascaded=4,block_orientation=-90, rotate = 2)
 virtual = viewport(device, width=142, height=8)
 
+isScreenOn = True
+
 def displayHour():
+    global isScreenOn
     hour = ""
     while True:
-        hour = datetime.datetime.now().strftime("%H:%M")
+        if (isScreenOn):
+            hour = datetime.datetime.now().strftime("%H:%M")
+        else:
+            hour = ""
         with canvas(virtual) as draw:
             text(draw, (0, 0), hour, fill="white", font=proportional(CP437_FONT))
         time.sleep(1)
+
+def toggleScreen(null):
+    global isScreenOn
+    start_time = time.time()
+    GPIO.wait_for_edge(16, GPIO.RISING)
+    time_elapsed = time.time() - start_time
+    if (time_elapsed >= 3):
+        isScreenOn = not isScreenOn
 
 if __name__ == "__main__":
     t1 = Thread(target = displayHour)
     t1.setDaemon(True)
     t1.start()
+
+    GPIO.add_event_detect(16, GPIO.FALLING, callback=toggleScreen, bouncetime=3000)
 
     try:
         while True:
