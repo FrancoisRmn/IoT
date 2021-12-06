7c3a8b9ad39c4a775ebf39e973331d0e863072b7
FrancoisRmn <francoisromain4@gmail.com>
2021-12-06 09:45:31 +0100

    Autorun the clock on start

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 # setup.sh
-echo [Desktop Entry] | sudo tee /etc/xdg/autostart/alarmclock.desktop
-echo Exec=mosquitto | sudo tee -a /etc/xdg/autostart/alarmclock.desktop
+echo [Desktop Entry] | sudo tee /etc/xdg/autostart/mosquitto.desktop
+echo Name=Mosquitto | sudo tee -a /etc/xdg/autostart/mosquitto.desktop
+echo Exec=mosquitto | sudo tee -a /etc/xdg/autostart/mosquitto.desktop
+
+echo [Desktop Entry] | sudo tee /etc/xdg/autostart/clock.desktop
+echo Type=Application | sudo tee -a /etc/xdg/autostart/clock.desktop
+echo Name=Clock | sudo tee -a /etc/xdg/autostart/clock.desktop
+echo Exec=/usr/bin/python3 /home/pi/IoT/src/raspberry/hour.py | sudo tee -a /etc/xdg/autostart/clock.desktop
