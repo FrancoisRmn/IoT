995b9d0c8119a4f6654a74d884dca7c6d4785586
FrancoisRmn <francoisromain4@gmail.com>
2021-12-06 11:19:05 +0100

    Autorun scala

--- setup.sh
+++ setup.sh
@@ -13,3 +13,8 @@ echo [Desktop Entry] | sudo tee /etc/xdg/autostart/python-api.desktop
 echo Type=Application | sudo tee -a /etc/xdg/autostart/python-api.desktop
 echo Name=PythonApi | sudo tee -a /etc/xdg/autostart/python-api.desktop
 echo Exec=/usr/bin/python3 /home/pi/IoT/src/raspberry/api.py | sudo tee -a /etc/xdg/autostart/python-api.desktop
+
+echo [Desktop Entry] | sudo tee /etc/xdg/autostart/scala.desktop
+echo Type=Application | sudo tee -a /etc/xdg/autostart/scala.desktop
+echo Name=Backend | sudo tee -a /etc/xdg/autostart/scala.desktop
+echo Exec=cd /home/pi/IoT/src/reveil-scala && sbt run | sudo tee -a /etc/xdg/autostart/scala.desktop
